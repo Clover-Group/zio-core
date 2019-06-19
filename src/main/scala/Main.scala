f8a15fa2c4382a337443a92d5953f1c83a2f048e
tampler <socnetfpga@gmail.com>
2019-06-19 18:17:36 +0300

    Started to integrate cats monad

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,20 @@
 package hello
 
+import scalaz.zio.{App, UIO}
+//import scalaz.zio.console.{putStrLn}
+
 object Main extends App {
+
+  def run(args: List[String]) =
+    res.fold(_ => 1, _ => 0)
   
+
+    val res = UIO.succeed(0)
+
+  /* val res:UIO[A] =
+    for {
+      data <- Program[F]
+
+    } yield() */
   
 }
--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -1,12 +1,24 @@
 package hello
 
-
 import cats.{Monad}
+import cats.implicits._
 
-sealed abstract class Program[F[_]:Monad] {
-  def run[A]:F[A]
+sealed abstract class Foo // ADT
+
+// Monadic class
+sealed abstract class Program[F[_]:Monad] { self => 
+  def run[A](in:A):F[A]
 }
 
 object Program {
-  
+  def apply[F[_]](implicit F:Program[F]):Program[F] = F
+
+  implicit val ListIntProgram = new Program[List] {
+    def run[Int](in:Int):List[Int] = List(in)
+  }      
+
+  implicit val ListFooProgram = new Program[List] {
+    def run[Foo](in:Foo) = List(in) 
+  }      
+
 }
